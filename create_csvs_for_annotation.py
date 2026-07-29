81cf5e8b544302f4ead9b409e701c5e8bc41f237
Julius Steuer <juliussteuer87@gmail.com>
2026-07-29 15:58:07 +0200

    add GUM-style annotation columns

--- create_csvs_for_annotation.py
+++ create_csvs_for_annotation.py
@@ -1,26 +1,30 @@
 from pathlib import Path
+import csv
 
 from conllu import parse_incr
+
+csv_header = ["story_id", "sent_id", "token_id", "deprel", "form", "GRP", "etype", "infstat", "minspan", "link", "identity"]
+
 
 if not Path("to_annotate").exists():
     Path("to_annotate").mkdir()
 
-current_story_id = "1"
+current_story_id = "0"
 sent_id_decr = 0
-story_file = None
+csvwriter = None
 
 with open("naturalstories/parses/ud/stories-aligned.conllx", "r") as f:
-    current_story_str = "story_id,sent_id,token_id,deprel,form\n"
     for sent_id, sentence in enumerate(parse_incr(f)):
         for token in sentence:
             # split only once as there could be subtokens
             story_id, token_id = token["misc"]["TokenId"].split(".", 1)
             # save to file if end of story encountered
             if story_id != current_story_id:
-                with open(f"to_annotate/story_{current_story_id}_coref.csv", "w") as f_story:
-                    f_story.write(current_story_str)
+                f_story = open(f"to_annotate/story_{current_story_id}_coref.csv", "w")
+                csvwriter = csv.writer(f_story, delimiter=";", quoting=csv.QUOTE_MINIMAL)
+                csvwriter.writerow(csv_header)
+
                 sent_id_decr = sent_id
                 current_story_id = story_id
-                current_story_str = "story_id;sent_id;token_id;deprel;form\n"
 
-            current_story_str += f"{story_id};{sent_id-sent_id_decr};{token_id};{token['deprel']};{token['form']}\n"
+            csvwriter.writerow([story_id, sent_id-sent_id_decr, token_id, token["deprel"], token["form"]])
