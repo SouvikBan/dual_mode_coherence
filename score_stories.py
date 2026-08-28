fb84aa9b04e0e47e79ecdd606e965fe7d96bbfe2
Julius Steuer <juliussteuer87@gmail.com>
2026-08-28 14:46:22 +0200

    base surprisal scoring on ud parse

--- score_stories.py
+++ score_stories.py
@@ -1,14 +1,10 @@
 from argparse import ArgumentParser
 from minicons import scorer
-import numpy as np
 import csv
-from collections import defaultdict
 from pathlib import Path
+import pandas as pd
 import torch
 from tqdm import tqdm
-
-import re
-
 from typing import List, Tuple
 
 device = "cpu"
@@ -18,8 +14,8 @@ elif torch.mps.is_available():
 	device = "mps"
 print(f"Using device: {device}")
 
-NATURAL_STORIES_TOK_FILE = "naturalstories/naturalstories_RTS/all_stories.tok"
-
+# NATURAL_STORIES_TOK_FILE = "naturalstories/naturalstories_RTS/all_stories.tok"
+NATURAL_STORIES_CONLLX_FILE = "naturalstories/parses/ud/stories-aligned.conllx"
 
 def parse_args():
 	parser = ArgumentParser()
@@ -65,6 +61,44 @@ def parse_args():
 # 		scores.extend(batch_scores)
 # 	return scores
 
+def _load_naturalstories_conllx(conllx_path):
+	"""Load naturalstories with sentence splitting info from ud parse."""
+	grouped_sents = []
+	with open(conllx_path, "r", encoding="utf-8") as f_conllx:
+		current_sentence_id = 1
+		last_item = 1
+		current_tokens, current_items, current_zones = [], [], []
+		for line in f_conllx.read().split("\n")[:-1]:
+			# empty lines signal sentence boundaries
+			if not line:
+				last_item = current_items[-1]
+				for t, z in zip(current_tokens, current_zones):
+					grouped_sents.append([
+						int(last_item), t, int(z), current_sentence_id
+					])
+				current_sentence_id += 1
+				current_tokens, current_items, current_zones = [], [], []
+			else:
+				line_content = line.split("\t")
+				token = line_content[1]
+				misc = line_content[-1].split(".")
+				item, zone = misc[:2]
+				item = item[8:]
+				if last_item != item:
+					current_sentence_id = 1
+				current_tokens.append(token)
+				current_items.append(item)
+				current_zones.append(zone)
+
+	# reassemble punctuation by grouping uniquely by item, zone and sentence, 
+	# then aggregating partial token strings and sorting
+	df = pd.DataFrame(data=grouped_sents, columns=["item", "token", "zone", "sentence_id"]) \
+			.groupby(["item", "zone", "sentence_id"])["token"] \
+			.agg("".join).reset_index() \
+			.sort_values(by=["item", "sentence_id", "zone"], axis=0)
+
+	return df
+
 def _get_out_file_name(args):
 	out_file_prefix = f"{args.model_name_or_path.split("/")[-1]}"
 	if args.revision:
@@ -78,7 +112,7 @@ def _get_out_file_name(args):
 
 
 def _get_word_scores(token_scores: List[Tuple[str, float]]) -> List[float]:
-	"""Assumes a GPT-2-style tokenizer, i.e., 'Ġ' is expected to mark the beginning of words"""
+	"""Assumes a GPT-2-style tokenizer, i.e., 'Ġ' is expected to mark the beginning of words."""
 	word_scores = []
 	current_word_score = 0.
 	for idx in range(len(token_scores)):
@@ -92,12 +126,12 @@ def _get_word_scores(token_scores: List[Tuple[str, float]]) -> List[float]:
 def main():
 
 	args = parse_args()
-	from transformers import AutoTokenizer
-	tokenizer = AutoTokenizer.from_pretrained(args.model_name_or_path)
-	# should use revision="step1000"
+	# One should use revision="step1000" for pythia models (equals 2B training tokens)
 	ilm_scorer = scorer.IncrementalLMScorer(
-		model=args.model_name_or_path, tokenizer=tokenizer, device=device, revision=args.revision)
+		model=args.model_name_or_path, device=device, revision=args.revision)
 	tokenizer = ilm_scorer.tokenizer
+
+	stories_df = _load_naturalstories_conllx(NATURAL_STORIES_CONLLX_FILE)
 
 	if not Path(args.output_dir).exists():
 		Path(args.output_dir).mkdir(parents=True)
@@ -109,24 +143,20 @@ def main():
 	csv_header = ["word", "item", "zone", "sentence_id", "sentence_position", "surprisal"]
 	csvwriter = csv.writer(f_out, delimiter="\t", quoting=csv.QUOTE_MINIMAL)
 	csvwriter.writerow(csv_header)
-		
-	with open(NATURAL_STORIES_TOK_FILE, "r") as f:
-		stories = f.read().split("\n")[1:-1]
 
-	words_by_story = defaultdict(list)
-	for line in stories:
-		word, _, story_id = line.split("\t")
-		words_by_story[story_id].append(word)
-
-	for story_id, story_words in tqdm(words_by_story.items()):
+	for story_id in tqdm(stories_df["item"].unique(), desc="Scording naturalstories"):
+		words, zones, items, sent_ids = \
+			stories_df[stories_df.item == story_id].token.tolist(), \
+			stories_df[stories_df.item == story_id].zone.tolist(), \
+			stories_df[stories_df.item == story_id].item.tolist(), \
+			stories_df[stories_df.item == story_id].sentence_id.tolist()
 
 		# no window size provided -> assume that stories fit in the context of the model
 		if not args.window_size:
-			story_words.insert(0, tokenizer.bos_token)
-			token_scores = ilm_scorer.token_score(" ".join(story_words))
-			word_scores = _get_word_scores(token_scores[0]) # skip bos token
+			token_scores = ilm_scorer.token_score(" ".join([tokenizer.bos_token] + words))
+			word_scores = _get_word_scores(token_scores[0])[1:] # skip bos token
 			# make sure no token was lost on the way
-			assert len(word_scores) == len(story_words), len(story_words) -len(word_scores)
+			assert len(word_scores) == len(words), len(words) - len(word_scores)
 
 		else:
 			# for now complain
@@ -139,15 +169,8 @@ def main():
 			# token_scores = _score(stimuli, scorer=ilm_scorer, batch_size=args.batch_size)
 			# word_scores = [_get_word_score(stimulus_score[1:]) for stimulus_score in token_scores]
 
-		# save with sentence ids and sentence positions
-		sentence_id = 1
-		sentence_position = 1
-		for story_position, (word, score) in enumerate(zip(story_words[1:], word_scores[1:]), start=1):
-			csvwriter.writerow([word, story_id, story_position, sentence_id, sentence_position, score])
-			sentence_position += 1
-			if re.search(r"[.!?]'?$", word) and not word in ["Mr.", "Dr."]:
-				sentence_id += 1
-				sentence_position = 1
+		for word, zone, item, sent_id, score in zip(words, zones, items, sent_ids, word_scores):
+			csvwriter.writerow([word, item, zone, sent_id, score])
 
 
 if __name__ == "__main__":
