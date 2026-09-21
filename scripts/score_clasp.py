d3ef0245513d17b99d9aa30b14c7be771254ac24
Julius Steuer <juliussteuer87@gmail.com>
2026-09-21 09:18:08 +0200

    updated CLASP preprocessing and surprisal script

--- scripts/score_clasp.py
+++ scripts/score_clasp.py
@@ -1,6 +1,7 @@
 # /// script
 # requires-python = ">=3.12"
 # dependencies = [
+#     "bitsandbytes>=0.46.1",
 #     "minicons>=0.3.39",
 # ]
 # ///
@@ -11,16 +12,24 @@ import pandas as pd
 import torch
 from tqdm import tqdm
 
+quantization_config = None
 device = "cpu"
 if torch.cuda.is_available():
 	device = "cuda"
 elif torch.mps.is_available():
 	device = "mps"
+	# from transformers import BitsAndBytesConfig
+	# quantization_config = BitsAndBytesConfig(
+	# 	load_in_8bit=True,
+	# )
 print(f"Using device: {device}")
+print(f"Using quantization: {quantization_config}")
 
 
 CLASP_FILE = "BLL2018/data/processed_ratings.csv"
-CLASP_WITH_CONTEXT_PREPROCESSED = "BLL2018/data/clasp_by_worker_and_sent.csv"
+CLASP_WITH_CONTEXT_PREPROCESSED = "BLL2018/data/clasp_with_context_by_worker_and_sent.csv"
+CLASP_NO_CONTEXT_PREPROCESSED = "BLL2018/data/clasp_no_context_by_worker_and_sent.csv"
+
 
 def parse_args():
 	parser = ArgumentParser()
@@ -57,45 +66,54 @@ def _get_out_file_name(args):
 # 	return [-1*s for s in word_scores]
 
 
+def _score_clasp(
+		clasp_path: str,
+		ilm_scorer: scorer.IncrementalLMScorer, \
+		output_dir: str, 
+		out_file_name: str
+	):
+
+	clasp_df = pd.read_csv(clasp_path)
+
+	output_dir_context = Path(output_dir) / Path(clasp_path).stem
+	if not output_dir_context.exists():
+		output_dir_context.mkdir(parents=True)
+
+	sentence_scores = []
+	for _, row in tqdm(clasp_df.iterrows()):
+		stimulus = row["sent"]
+		prefix = f"{ilm_scorer.tokenizer.bos_token} {row["prefix"]}" \
+			if "prefix" in row else f"{ilm_scorer.tokenizer.bos_token}"
+		sentence_score = ilm_scorer.conditional_score(
+			prefix, stimulus, 
+			# we want summed sentence surprisals
+			reduction=lambda x: x.sum().item(),
+			# thus base two logarithm 
+			base_two=True, 
+			# and Tiago's bow correction
+			bow_correction=True
+		)
+		sentence_scores.append(-1*sentence_score[0])
+
+	clasp_with_context_df = clasp_df.assign(sentence_surprisal = sentence_scores)
+	clasp_with_context_df.to_csv(output_dir_context / out_file_name, sep="\t")
+
+
 def main():
 
 	args = parse_args()
 	# One should use revision="step1000" for pythia models (equals 2B training tokens)
 	ilm_scorer = scorer.IncrementalLMScorer(
-		model=args.model_name_or_path, device=device, revision=args.revision)
-	tokenizer = ilm_scorer.tokenizer
+		model=args.model_name_or_path, device=device, revision=args.revision, 
+		#quantization_config=quantization_config, device_map="auto"
+	)
 
-	clasp_df = pd.read_csv(CLASP_WITH_CONTEXT_PREPROCESSED) \
-		.drop(["suffix", "suffixextra"], axis=1)
+	out_file_name = _get_out_file_name(args)
 
-	sentence_scores = []
-	for i, row in tqdm(clasp_df.iterrows()):
-		stimulus = row["sent"]
-		prefix = f"{tokenizer.bos_token} {row["prefix"]}"
-		# print(f"{prefix} {stimulus}")
+	_score_clasp(CLASP_WITH_CONTEXT_PREPROCESSED, ilm_scorer, args.output_dir, out_file_name)
+	_score_clasp(CLASP_NO_CONTEXT_PREPROCESSED, ilm_scorer, args.output_dir, out_file_name)
 
-		# no window size provided -> assume that stories fit in the context of the model
-		if not args.window_size:
-			sentence_score = ilm_scorer.conditional_score(
-				prefix, stimulus, 
-				# we want summed sentence surprisals
-				reduction=lambda x: x.sum().item(),
-				# thus base two logarithm 
-				base_two=True, 
-				# and Tiago's bow correction
-				bow_correction=True
-			)
-			sentence_scores.append(-1*sentence_score[0])
-		else:
-			# for now complain
-			raise Exception
-
-	if not Path(args.output_dir).exists():
-		Path(args.output_dir).mkdir(parents=True)
-	out_file_name = _get_out_file_name(args)	
-
-	clasp_df = clasp_df.assign(sentence_surprisal = sentence_scores)
-	clasp_df.to_csv(Path(args.output_dir) / out_file_name, sep="\t")
+	
 
 if __name__ == "__main__":
 	main()
